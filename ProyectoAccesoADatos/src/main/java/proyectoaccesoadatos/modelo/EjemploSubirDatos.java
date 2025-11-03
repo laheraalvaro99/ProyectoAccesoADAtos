0717a1a8616528b8ff4950252004cbacd7c67170
gsytnik <gsytnik@larioja.edu.es>
2025-11-03 09:13:07 +0100

    Mod proyecto

--- ProyectoAccesoADatos/src/main/java/proyectoaccesoadatos/modelo/EjemploSubirDatos.java
+++ ProyectoAccesoADatos/src/main/java/proyectoaccesoadatos/modelo/EjemploSubirDatos.java
@@ -4,10 +4,41 @@
  */
 package proyectoaccesoadatos.modelo;
 
-/**
- *
- * @author Diurno
- */
-public class NewClass {
-    
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EjemploSubirDatos {
+public static void main(String[] args) {
+        try {
+Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://localhost:1433;"
+           + "databaseName=Academia;"
+           + "integratedSecurity=true;"
+           + "encrypt=false;"
+           + "trustServerCertificate=true;";
+Connection conexion = DriverManager.getConnection(url);
+
+        Statement sentencia = conexion.createStatement();
+        String sql = "select id,id_user_alumno,id_user_profesor,id_asignatura,puntuacion\n" +
+"from notas";
+        ResultSet resul = sentencia.executeQuery(sql);
+
+        while (resul.next()) {
+      System.out.println(resul.getInt(1) + ", " + resul.getInt(2) + ", " + resul.getInt(3) + ", " + resul.getInt(4)+", "+resul.getDouble(5));
+            
+        }
+        resul.close();
+        sentencia.close(); 
+        conexion.close(); 
+        } catch (SQLException e) {
+        e.printStackTrace();
+}       catch (ClassNotFoundException ex) {     
+            Logger.getLogger(ProyectoTema2.class.getName()).log(Level.SEVERE, null, ex);
+        }     
+    }
 }
