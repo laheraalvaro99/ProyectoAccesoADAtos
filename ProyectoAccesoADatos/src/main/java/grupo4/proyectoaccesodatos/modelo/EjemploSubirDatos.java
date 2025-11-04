23d59ecb2d0e6c297e5f0771196955f45169e73b
gsytnik <gsytnik@larioja.edu.es>
2025-11-04 09:03:21 +0100

    Mod proyecto

--- ProyectoAccesoADatos/src/main/java/grupo4/proyectoaccesodatos/modelo/EjemploSubirDatos.java
+++ ProyectoAccesoADatos/src/main/java/grupo4/proyectoaccesodatos/modelo/EjemploSubirDatos.java
@@ -11,30 +11,31 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class EjemploSubirDatos {
-public static void main(String[] args) {
+
+    public static void main(String[] args) {
         try {
-Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String url = "jdbc:sqlserver://localhost:1433;"
-           + "databaseName=academia;"
-           + "integratedSecurity=true;"
-           + "encrypt=false;"
-           + "trustServerCertificate=true;";
-Connection conexion = DriverManager.getConnection(url);
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://localhost:1433;"
+                    + "databaseName=academia;"
+                    + "integratedSecurity=true;"
+                    + "encrypt=false;"
+                    + "trustServerCertificate=true;";
+            Connection conexion = DriverManager.getConnection(url);
 
-        Statement sentencia = conexion.createStatement();
-        String sql = "select id,id_user_alumno,id_user_profesor,id_asignatura,puntuacion\n" +
-"from notas";
-        ResultSet resul = sentencia.executeQuery(sql);
+            Statement sentencia = conexion.createStatement();
+            String sql = "select id,id_user_alumno,id_user_profesor,id_asignatura,puntuacion\n"
+                    + "from notas";
+            ResultSet resul = sentencia.executeQuery(sql);
 
-        while (resul.next()) {
-      System.out.println(resul.getInt(1) + ", " + resul.getInt(2) + ", " + resul.getInt(3) + ", " + resul.getInt(4)+", "+resul.getDouble(5));
-            
-        }
-        resul.close();
-        sentencia.close(); 
-        conexion.close(); 
+            while (resul.next()) {
+                System.out.println(resul.getInt(1) + ", " + resul.getInt(2) + ", " + resul.getInt(3) + ", " + resul.getInt(4) + ", " + resul.getDouble(5));
+
+            }
+            resul.close();
+            sentencia.close();
+            conexion.close();
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
-}     
+        }
     }
 }
