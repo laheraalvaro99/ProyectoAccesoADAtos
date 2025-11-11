7081c0360017a9b0afc8d5571a83c4cdcc3c40b0
argudoplomo <davidargudopicas@gmail.com>
2025-11-11 10:16:59 +0100

    Delete proyectADConectores/proyectADConectores directory

--- proyectADConectores/proyectADConectores/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.proyectadconectores {
-    requires javafx.controls;
-    requires javafx.fxml;
-				requires java.desktop;
-
-    opens controladores to javafx.fxml;
-    exports controladores;
-}
